d06b513ebce84b0c044f000f93c9f8a075745afe
Jakub Jirous <info@jakubjirous.cz>
2023-02-28 11:45:29 -0800

    [icons] add spinner icon for loading state

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
     }),
     animation: {
       none: "none",
-      spin: "spin 1s linear infinite",
+      spin: "spin 500ms linear infinite",
       ping: "ping 1s cubic-bezier(0, 0, 0.2, 1) infinite",
       pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       bounce: "bounce 1s infinite",
