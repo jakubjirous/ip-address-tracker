bbedb8ca6939eba2cc6e2ba742205e308b305144
Jakub Jirous <info@jakubjirous.cz>
2023-02-28 11:41:56 -0800

    [app] remove suspense for react-query
    [app] remove suspense for react-query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,16 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { ReactNode, useState } from "react";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            suspense: true,
-          },
-        },
-      })
-  );
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
