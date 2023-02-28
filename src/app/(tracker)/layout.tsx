dca2091c1901d3476967d39dc7898768707dfdcf
Jakub Jirous <info@jakubjirous.cz>
2023-02-28 14:57:28 -0800

    [app] grid layout for tracker

--- src/app/(tracker)/layout.tsx
+++ src/app/(tracker)/layout.tsx
@@ -3,7 +3,11 @@
 import React, { ReactNode } from "react";
 
 const TrackerLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <section className="grid h-full grid-cols-1 grid-rows-40 md:grid-rows-35">
+      {children}
+    </section>
+  );
 };
 
 export default TrackerLayout;
