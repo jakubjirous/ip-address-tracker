38cf0ac8d4e2633dde10578917669660120564fa
Jakub Jirous <info@jakubjirous.cz>
2023-03-01 08:31:57 -0800

    [components] map marker and viewport tweak

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,26 +1,44 @@
 import { useMapState } from "@/hooks";
 import { LocationIcon } from "@/icons";
 import "mapbox-gl/dist/mapbox-gl.css";
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
 
 export const Map: FC = () => {
-  const [mapViewport] = useMapState();
+  const {
+    map: [mapViewport, setMapViewport],
+    marker: [makerViewport],
+  } = useMapState();
+
+  const { longitude, latitude, zoom } = useMemo(() => {
+    return {
+      longitude: 10.69404926798569,
+      latitude: 41.00972811395383,
+      zoom: 0.882812691030048,
+    };
+  }, []);
 
   return (
     <ReactMapGL
-      initialViewState={{ ...mapViewport }}
+      initialViewState={{
+        longitude,
+        latitude,
+        zoom,
+      }}
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_SECRET_KEY}
       mapStyle="mapbox://styles/mapbox/streets-v12"
+      onMove={(event) => setMapViewport(event.viewState)}
       {...mapViewport}
     >
-      <Marker
-        latitude={mapViewport.latitude}
-        longitude={mapViewport.longitude}
-        offset={[18, -55]}
-      >
-        <LocationIcon />
-      </Marker>
+      {makerViewport && (
+        <Marker
+          latitude={makerViewport?.latitude}
+          longitude={makerViewport?.longitude}
+          offset={[18, -55]}
+        >
+          <LocationIcon />
+        </Marker>
+      )}
     </ReactMapGL>
   );
 };
