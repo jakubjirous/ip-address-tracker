9249ed290b1abbf7f953eb4e0eb7d1caf2340f6b
Jakub Jirous <info@jakubjirous.cz>
2023-03-01 08:32:29 -0800

    [hooks] disable refetch on window focus

--- src/hooks/use-geolocation/use-geolocation.tsx
+++ src/hooks/use-geolocation/use-geolocation.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchGeolocation = async (search: string): Promise<GeolocationType> => {
-  const { ipAddress, domain } = detectIpOrDomain(search);
+  const { ipAddress, domain } = detectIpOrDomain(search.trim());
 
   const { data } = await axios.get(
     `${process.env.NEXT_PUBLIC_GEOLOCATION_API_URL}?apiKey=${process.env.NEXT_PUBLIC_GEOLOCATION_SECRET_KEY}&ipAddress=${ipAddress}&domain=${domain}`
@@ -17,6 +17,7 @@ const useGeolocation = (search: string) => {
   return useQuery({
     queryKey: ["geolocation", search],
     queryFn: () => fetchGeolocation(search),
+    refetchOnWindowFocus: false,
     enabled: !!search,
   });
 };
